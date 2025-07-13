8c589a9ea49bcf98b089b300d589af9617858004
German S <germansantoandre@gmail.com>
2025-07-13 18:34:55 -0300

    FINAL

--- src/main/java/aed/BloqueTx.java
+++ src/main/java/aed/BloqueTx.java
@@ -8,18 +8,13 @@ public class BloqueTx implements Comparable<BloqueTx> {
 
     private int montoMedio;
     private int sumatoriaMontos;
-    private int cantTxUltBloque;
     private int cantTxSinCreacion;
 
     //O(1)
     public BloqueTx(){
         heapUltBloque   = new HeapSobreArrayList<>();
         ultBloque       = new ListaEnlazada<>();
-
-        montoMedio          = 0;
-        sumatoriaMontos     = 0;
-        cantTxUltBloque     = 0;
-        cantTxSinCreacion   = 0;
+        inicializarEstadisticas();
     }
 
     //O(2*n) -> O(n)
@@ -29,30 +24,44 @@ public class BloqueTx implements Comparable<BloqueTx> {
         heapUltBloque   = new HeapSobreArrayList<>();
         ultBloque       = new ListaEnlazada<>();
 
-        //como es un nuevo bloque, reseteo todas las variables
-        montoMedio          = 0;
-        sumatoriaMontos     = 0;
-        cantTxSinCreacion   = 0;
-        cantTxUltBloque     = transacciones.length;
+        //como es un nuevo bloque, inicializo todas las variables de la estadistica
+        inicializarEstadisticas();
 
-        for(int i = 0; i < cantTxUltBloque; i++){ // O(n)
+        for(int i = 0; i < transacciones.length; i++){ // O(n)
             handlesUltBloque.add(ultBloque.agregarAtras(transacciones[i]));    // O(1)
-
-            if(transacciones[i].id_comprador() != 0){
-                sumatoriaMontos += transacciones[i].monto();    //O(1)
-                cantTxSinCreacion++;                            //O(1)
-            }
+            modificarEstadisticas(transacciones[i], true);  //O(1)
         }
-        calcularMontoMedio(sumatoriaMontos, cantTxSinCreacion); //O(1)
+        calcularMontoMedio(); //O(1)
         heapUltBloque.Heapify(handlesUltBloque);    //heapificar con algoritmo floyd -> O(n)
     }
 
     //O(1)
-    private void calcularMontoMedio(int sum, int cant){
-        if(cant != 0)
-            montoMedio = sum/cant;  //O(1)
+    private void calcularMontoMedio(){
+        if(cantTxSinCreacion != 0)
+            montoMedio = sumatoriaMontos/cantTxSinCreacion;  //O(1)
         else
             montoMedio = 0;         //O(1)
+    }
+
+    //O(1)
+    private void inicializarEstadisticas(){
+        montoMedio          = 0;
+        sumatoriaMontos     = 0;
+        cantTxSinCreacion   = 0;
+    }
+
+    //O(1)
+    private void modificarEstadisticas(Transaccion tx, boolean sumar){
+        if(tx.id_comprador() != 0){
+            if(sumar == true){
+                sumatoriaMontos     += tx.monto();  //O(1)
+                cantTxSinCreacion   += 1;           //O(1)
+            }
+            else{
+                sumatoriaMontos     -= tx.monto();  //O(1)
+                cantTxSinCreacion   -= 1;           //O(1)
+            }                    
+        }
     }
 
     //O(1)
@@ -84,13 +93,8 @@ public class BloqueTx implements Comparable<BloqueTx> {
         handleTxEliminar.delete();   // O(1)
         heapUltBloque.SacarMaximo(); // reacomodar heap transacciones ult bloque -> O(log n)
 
-        if(txEliminar.id_comprador() != 0){
-            cantTxSinCreacion   -= 1; // O(1)
-            sumatoriaMontos     -= txEliminar.monto(); // O(1)
-        }
-        cantTxUltBloque -= 1; // O(1)
-
-        calcularMontoMedio(sumatoriaMontos, cantTxSinCreacion); // O(1)
+        modificarEstadisticas(txEliminar, false); // O(1)
+        calcularMontoMedio(); // O(1)
 
         return txEliminar;
     }
